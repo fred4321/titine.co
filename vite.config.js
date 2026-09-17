dbd151199830d5b0cd2f08309618c6a35e4dd5f7
fred4321 <f.fanien@gmail.com>
2026-09-17 18:11:35 +0200

    up

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import { resolve } from 'path'
 import { globSync } from 'glob'
+import ejs from 'ejs'
 
 const inputs = globSync('src/**/index.html').reduce((acc, file) => {
     const relativePath = file.replace(/^src\//, '')
@@ -10,12 +11,27 @@ const inputs = globSync('src/**/index.html').reduce((acc, file) => {
     return acc
 }, {})
 
+const renderPartials = {
+    name: 'render-html-partials',
+    enforce: 'pre',
+    transformIndexHtml: {
+        order: 'pre',
+        handler(html, context) {
+            return ejs.render(html, {}, {
+                filename: context.filename,
+                root: resolve(__dirname, 'src'),
+            })
+        },
+    },
+}
+
 export default defineConfig({
     // Si hébergé sur https://utilisateur.github.io/mon-projet/, décommente et ajuste la ligne suivante :
     // base: '/mon-projet/',
     root: 'src',
 
     plugins: [
+        renderPartials,
         createHtmlPlugin({
             minify: true,
         }),
